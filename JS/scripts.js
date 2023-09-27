bfefc0f8a0f4abd881fc39016d57b6ffe522d61b
bajrushivlora <bajrushivlora@gmail.com>
2023-09-27 08:38:00 -0400

    testing javascript

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,33 @@
+// function setHeroImage() {
+//     let photoGallery = [
+//       "IMAGES/image-01.jpg",
+//       "IMAGES/image-02.jpg",
+//       "IMAGES/image-03.jpg",
+//       "IMAGES/image-04.jpg",
+//       "IMAGES/image-05.jpg",
+//       "IMAGES/image-06.jpg"
+//     ];
+  
+//     var heroImage = document.querySelector(".section-hero");
+  
+//     var randomPhoto = photoGallery[Math.floor(Math.random() * photoGallery.length)];
+  
+//     heroImage.style.backgroundImage = `url(${randomPhoto})`;
+//     heroImage.style.maxHeight = `${window.innerHeight}px`;
+//   }
+  
+//   // Add an event listener for window resize
+//   window.addEventListener("resize", setHeroImage);
+  
+//   // Call the function when the page, including all resources, has fully loaded
+//   window.onload = setHeroImage;
+
+let debounceTimer;
+const debounceDelay = 250; // Adjust the debounce delay as needed
+
 function setHeroImage() {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(function () {
     let photoGallery = [
       "IMAGES/image-01.jpg",
       "IMAGES/image-02.jpg",
@@ -7,17 +36,18 @@ function setHeroImage() {
       "IMAGES/image-05.jpg",
       "IMAGES/image-06.jpg"
     ];
-  
+
     var heroImage = document.querySelector(".section-hero");
-  
+
     var randomPhoto = photoGallery[Math.floor(Math.random() * photoGallery.length)];
-  
+
     heroImage.style.backgroundImage = `url(${randomPhoto})`;
     heroImage.style.maxHeight = `${window.innerHeight}px`;
-  }
-  
-  // Add an event listener for window resize
-  window.addEventListener("resize", setHeroImage);
-  
-  // Call the function when the page, including all resources, has fully loaded
-  window.onload = setHeroImage;
+  }, debounceDelay);
+}
+
+// Add an event listener for window resize
+window.addEventListener("resize", setHeroImage);
+
+// Call the function when the page, including all resources, has fully loaded
+window.onload = setHeroImage;
