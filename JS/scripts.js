50acb1354ea30589152426778a9266fbb4ff39e9
bajrushivlora <bajrushivlora@gmail.com>
2023-09-26 21:55:15 -0400

    random photo javascript

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,35 @@
-let container = document.querySelector(".section-hero");
+let photoGallery = [
+    "IMAGES/image-01.jpg",
+    "IMAGES/image-02.jpg",
+    "IMAGES/image-03.jpg",
+    "IMAGES/image-04.jpg",
+    "IMAGES/image-05.jpg",
+    "IMAGES/image-06.jpg"
+  ];
 
-function mobileHeight() {
-    container.style.height = window.innerHeight;
+var heroImage = document.querySelector(".section-hero");
+
+var randomPhoto = photoGallery[Math.floor(Math.random()*photoGallery.length)];
+
+document.addEventListener("DOMContentLoaded", function(){
+    heroImage.style.backgroundImage = `url(${randomPhoto})`;
+});
+
+
+var mediaQuery = window.matchMedia("(max-width: 520px)");
+
+// Check if the media query matches
+if (mediaQuery.matches) {
+    heroImage.style.height = `${window.innerHeight}px`;
+} else {
+    heroImage.style.height = "100vh";
 }
+
+// Add an event listener to respond to changes in screen size
+mediaQuery.addListener(function (event) {
+  if (event.matches) {
+    heroImage.style.height = `${window.innerHeight}px`;
+  } else {
+    heroImage.style.height = "100vh";
+  }
+});
