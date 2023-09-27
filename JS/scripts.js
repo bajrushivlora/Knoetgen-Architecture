b1e7bfd6378ed750a69569e74676eeea9869298c
bajrushivlora <bajrushivlora@gmail.com>
2023-09-26 22:06:46 -0400

    more testing

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,35 +1,23 @@
-let photoGallery = [
-    "IMAGES/image-01.jpg",
-    "IMAGES/image-02.jpg",
-    "IMAGES/image-03.jpg",
-    "IMAGES/image-04.jpg",
-    "IMAGES/image-05.jpg",
-    "IMAGES/image-06.jpg"
-  ];
-
-var heroImage = document.querySelector(".section-hero");
-
-var randomPhoto = photoGallery[Math.floor(Math.random()*photoGallery.length)];
-
-document.addEventListener("DOMContentLoaded", function(){
+function setHeroImage() {
+    let photoGallery = [
+      "IMAGES/image-01.jpg",
+      "IMAGES/image-02.jpg",
+      "IMAGES/image-03.jpg",
+      "IMAGES/image-04.jpg",
+      "IMAGES/image-05.jpg",
+      "IMAGES/image-06.jpg"
+    ];
+  
+    var heroImage = document.querySelector(".section-hero");
+  
+    var randomPhoto = photoGallery[Math.floor(Math.random() * photoGallery.length)];
+  
     heroImage.style.backgroundImage = `url(${randomPhoto})`;
-});
-
-
-var mediaQuery = window.matchMedia("(max-width: 520px)");
-
-// Check if the media query matches
-if (mediaQuery.matches) {
-    heroImage.style.height = `${window.innerHeight}px`;
-} else {
-    heroImage.style.height = "100vh";
-}
-
-// Add an event listener to respond to changes in screen size
-mediaQuery.addListener(function (event) {
-  if (event.matches) {
-    heroImage.style.height = `${window.innerHeight}px`;
-  } else {
-    heroImage.style.height = "100vh";
+    heroImage.style.maxHeight = `${window.innerHeight}px`;
   }
-});
+  
+  // Add an event listener for window resize
+  window.addEventListener("resize", setHeroImage);
+  
+  // Call the function when the page, including all resources, has fully loaded
+  window.onload = setHeroImage;
